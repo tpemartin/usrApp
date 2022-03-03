075561e72f6ffc6f9ca0354caa2ad2b8302246f2
tpemartin <tpemartin@gmail.com>
2022-03-03 19:11:15 +0800

    update

--- fig_pageStory.R
+++ fig_pageStory.R
@@ -1,7 +1,11 @@
 fig <- Fig()
 fig$export("assets/css/pageStory")
-fig$split_css$`pageStory-navbar-logo`$remain$background <- "url(lib/img-1/logo.png)"
-
+fig$split_css$`pageStory-navbar-logo`$remain$background <- "url(/lib/img-1/logo.png)"
+fig$split_css$`pageStory-navbar-logo`$inside_autoLayout$margin <- NULL
+fig$split_css$`pageStory-navbar-iconMore`$inside_autoLayout$margin <- NULL
+fig$split_css$`pageStory-navbar-iconMore`$remain$background <- "url(/lib/img-1/iconMore.svg)"
+fig$split_css$pageStory$remain$width <- "100%"
+fig$split_css$pageStory$remain$height="100vh"
 fig$update_css()
 fig$export("assets/css/pageStory")
 fig$ui() |> browseTag2()
