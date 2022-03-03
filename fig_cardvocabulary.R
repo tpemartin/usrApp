fb00473e478804d0ce03a3d342f323775f8864d0
tpemartin <tpemartin@gmail.com>
2022-03-03 20:12:45 +0800

    step 4: fix icon padding

--- fig_cardvocabulary.R
+++ fig_cardvocabulary.R
@@ -2,5 +2,6 @@ fig <- Fig()
 
 fig$split_css$`card-content-teacher-word`$inside_autoLayout$flex <- NULL
 fig$split_css$`card-content-teacher-img`$remain$background = "url(/lib/img-1/teacher.png)"
+fig$split_css$`card-background-icons`$auto_layout$padding = "16px"
 fig$update_css()
 fig$export("assets/css/ecard")
