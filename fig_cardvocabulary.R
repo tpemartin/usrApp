6658c810eee1c5a7da0d9378f3126e80f0491764
tpemartin <tpemartin@gmail.com>
2022-03-03 19:55:43 +0800

    step 4: fix image in cardContentTeacher

--- fig_cardvocabulary.R
+++ fig_cardvocabulary.R
@@ -1,5 +1,6 @@
 fig <- Fig()
 
 fig$split_css$`card-content-teacher-word`$inside_autoLayout$flex <- NULL
+fig$split_css$`card-content-teacher-img`$remain$background = "url(/lib/img-1/teacher.png)"
 fig$update_css()
 fig$export("assets/css/ecard")
