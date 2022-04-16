9870831a04b6605fef991147750fe868139e7117
tpemartin <tpemartin@gmail.com>
2022-04-16 09:46:29 +0800

    Update btnPress.js

--- inst/usrapp/js/btnPress.js
+++ inst/usrapp/js/btnPress.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  btnSignup=document.querySelector("body > div");
-  btnSignup.addEventListener("mousedown", btnpress);
-  btnSignup.addEventListener("mouseup", btnunpress);
+  //btnSignup=document.querySelector("body > div");
+  //btnSignup.addEventListener("mousedown", btnpress);
+  //btnSignup.addEventListener("mouseup", btnunpress);
+
+  btnSignupS=document.getElementsByClassName("btnSignup");
+  for(let i=0; i < btnSignupS.length; i++){
+    attachPressEventListener(btnSignupS[i]);
+  };
 });
 function btnpress(){
-    btnSignup.classList.add("pressed")
+    this.classList.add("pressed") //btnSignup
 }
 function btnunpress(){
-    btnSignup.classList.remove("pressed");
+    this.classList.remove("pressed");//btnSignup
+}
+function attachPressEventListener(btnSignup){
+  btnSignup.addEventListener("mousedown", btnpress);
+  btnSignup.addEventListener("mouseup", btnunpress);
 }
